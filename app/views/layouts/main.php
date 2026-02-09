7781d6800d1ff40cf8de78e527bc63bd12ea5302
manh2903 <manhdz22pl@gmail.com>
2026-02-09 11:43:53 +0700

    a

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -27,7 +27,6 @@ mb_internal_encoding('UTF-8');
   <title><?= htmlspecialchars($title ?? 'Client') ?></title>
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link href="<?= $base ?>/assets/css/app.css" rel="stylesheet">
   <link href="<?= $base ?>/assets/css/home.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
   <script src="<?= $base ?>/assets/js/app.js"></script>
