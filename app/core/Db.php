1d23878c7233f4ed51d954f556a29bf8e68830fb
dawn <bqm3010@gmail.com>
2026-03-09 11:53:09 +0700

    test

--- app/core/Db.php
+++ app/core/Db.php
@@ -30,7 +30,7 @@ class Db {
         $options = [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => false,
+            PDO::ATTR_EMULATE_PREPARES   => true,
         ];
         
         try {
